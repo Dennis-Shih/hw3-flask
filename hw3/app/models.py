ca6ae525a6c838d1853ca54af36a72b23cfb365e
Dennis Shih <dennisshih89@gmail.com>
2021-04-27 03:22:39 -0700

    debug and testing a bit

--- hw3/app/models.py
+++ hw3/app/models.py
@@ -5,11 +5,10 @@ class User(db.Model):
     # id (int)
     # author (string, unique, can't be null)
     # message (linkd to Messages table)
-	id=db.Column(db.Integer, primary_key=True)
-	author=db.Column(db.String(70), unique=True, nullable=False)
-
-	message=db.Column(db.String(200), db.ForeignKey('messages.id')
-
+    id=db.Column(db.Integer, primary_key=True)
+    author=db.Column(db.String(70), unique=True, nullable=False)
+    message=db.Column(db.String(200), db.ForeignKey('messages.id'))
+    
     def __repr__(self):
         return f'<User {self.author}>'
 
@@ -18,12 +17,14 @@ class Messages(db.Model):
     # id (int)
     # message (string, not unique, can't be null)
     # user_id link to id (int)
-	id=db.Column(db.Integer, primary_key=True)
-	message=db.Column(db.String(200, unique=False, nullable=False)
-	user_id=db.Column(db.Integer, db.ForeignKey('user.id'))
+    id=db.Column(db.Integer, primary_key=True)
+    message=db.Column(db.String(200), unique=False, nullable=False)
+    user_id=db.Column(db.Integer, db.ForeignKey('user.id'))
 
     # write __repr__ that outputs
     # <Message: MESSAGE_GOES_HERE>
     # replace MESSAGE_GOES_HERE with the message
-	def __repr__(self):
-		return '<Message: {self.message}>'
+    def __repr__(self):
+        return '<Message: {self.message}>'
+
+db.create_all()
--- hw3/app/routes.py
+++ hw3/app/routes.py
@@ -9,22 +9,28 @@ from app.models import User, Messages
 @app.route('/', methods=['GET', 'POST'])
 def home():
     form=MessageForm()
+
     if form.validate_on_submit():
+        exists = False
         # check if user exits in database
         # if not create user and add to database
-        # create row in Message table with user (created/found) add to ta database
-
-	users= User.query.all()
-	exists=False
-	for u in users:
-		if u.author == form.author:
-			exists=true
-			break
-	if exists == False:
-		u=User(author=u.author)
-		db.session.add(u)
-		db.session.commit()
-
+        # create row in Message table with user (created/found) add to tausers =  User.query.all()
+        users =User.query.all()
+        the_user = None
+        for u in users:
+            if u.author == form.author:
+                exists=true
+                the_user=u
+                break
+        if not exists:
+            the_user=User(author=form.author.data)
+            db.session.add(the_user)
+            db.session.commit()
+        print(the_user)
+        m=Messages(message=form.message.data,user_id=the_user.id)
+        db.session.add(m)
+        db.session.commit()
+        
 
     # output all messages
     # create a list of dictionaries with the following structure
@@ -34,6 +40,14 @@ def home():
     {'author':'carlos','message':'Yo! Where you at?!'},
     {'author':'Jerry','message':'Home. You?'}
     ]
-
-    return render_template('home.html', posts=posts, form=form)
+    messages=Messages.query.all()
+    messageList = []
+    for m in messages:
+        print(m)
+        user=User.query.filter_by(id=m.user_id).first()
+        if (user is None):
+            continue
+        name=user.author
+        messageList.append({'author':name, 'message':m.message})
+    return render_template('home.html', posts=messageList, form=form)
 
